1a9d29ded0237f4adec11306cbf3bef86d84c5d1
Yeek Zhang <yeekzhang@sgamecom.com>
2011-11-24 17:56:55 +0800

    Passed the KDELIBS compliation

--- package/kde/kdelibs/kdelibs.mk
+++ package/kde/kdelibs/kdelibs.mk
@@ -8,17 +8,18 @@ KDELIBS_SITE = git://anongit.kde.org/kdelibs
 
 KDELIBS_EXTERNAL_BUILD = YES
 KDELIBS_INSTALL_STAGING = YES
-KDELIBS_INSTALL_TARGET = NO
 KDELIBS_AUTORECONF = NO
+
 #KDELIBS_CONF_ENV = 
-KDELIBS_CONF_OPT = -DQT_DBUSXML2CPP_EXECUTABLE="/usr/bin/qdbusxml2cpp" \
-		-DQT_DBUSCPP2XML_EXECUTABLE="/usr/bin/qdbuscpp2xml"
+KDELIBS_CONF_OPT = \
+	-DQT_DBUSXML2CPP_EXECUTABLE="/usr/bin/qdbusxml2cpp" \
+	-DQT_DBUSCPP2XML_EXECUTABLE="/usr/bin/qdbuscpp2xml" \
+	-D_SDO_CONFIG_DIR=$(STAGING_DIR)/usr
 
 #KDELIBS_DEPENDENCIES = kerberos(non-cross-compiling) hupnp openexr
 KDELIBS_DEPENDENCIES = qt attica avahi aspell akonadi dbusmenu-qt enchant hspell automoc4 phonon fam jasper \
 		qimageblitz soprano strigi pcre libungif jpeg libpng libxslt oscaf \
-		udev xz qca2 acl docbook-xml docbook-xsl grantlee
+		udev xz qca2 acl docbook-xml docbook-xsl grantlee polkit-qt-1
 
 $(eval $(call CMAKETARGETS))
-
 
