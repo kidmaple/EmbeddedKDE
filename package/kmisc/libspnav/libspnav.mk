b332b1e55e9dd8c8569b514d2341a026b01c5b6c
Yeek Zhang <yeekzhang@sgamecom.com>
2011-12-01 15:06:33 +0800

    Fixed libspnav cross-compiling problem

--- package/kmisc/libspnav/libspnav.mk
+++ package/kmisc/libspnav/libspnav.mk
@@ -6,9 +6,10 @@
 LIBSPNAV_VERSION = 0.2.2
 LIBSPNAV_SOURCE = libspnav-$(LIBSPNAV_VERSION).tar.gz
 LIBSPNAV_SITE = http://kent.dl.sourceforge.net/project/libspnav/libspnav/libspnav-$(LIBSPNAV_VERSION)
+
 LIBSPNAV_DEPENDENCIES =
 LIBSPNAV_INSTALL_STAGING = YES
-LIBSPNAV_INSTALL_TARGET = NO
+LIBSPNAV_INSTALL_TARGET = YES
 
 define LIBSPNAV_CONFIGURE_CMDS
 	(cd $(@D); rm -rf config.cache; \
@@ -19,12 +20,12 @@ define LIBSPNAV_CONFIGURE_CMDS
 		--target=$(GNU_TARGET_NAME) \
 		--host=$(GNU_TARGET_NAME) \
 		--build=$(GNU_HOST_NAME) \
-		--prefix=$(STAGING_DIR)/usr \
+		--prefix=/usr \
 	)
 endef
 
 define LIBSPNAV_BUILD_CMDS
-        $(MAKE) -C $(@D)
+        $(MAKE) CC=$(TARGET_CC) AR=$(TARGET_AR) -C $(@D)
 endef
 
 define LIBSPNAV_INSTALL_STAGING_CMDS
@@ -32,7 +33,7 @@ define LIBSPNAV_INSTALL_STAGING_CMDS
 endef
 
 define LIBSPNAV_INSTALL_TARGET_CMDS
-	( )
+	$(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) install
 endef
 
 $(eval $(call GENTARGETS))
